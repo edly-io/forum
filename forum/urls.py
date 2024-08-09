92c473073448d0c4c3f49fb0681d1bfebc69aa93
Ali-Salman29 <ali.salman@arbisoft.com>
2024-08-09 11:03:54 +0200

    fix: match urls with the forum app

--- forum/urls.py
+++ forum/urls.py
@@ -9,6 +9,6 @@ from forum.views.votes import CommentVoteView, ThreadVoteView
 
 urlpatterns = [
     path("forum_proxy<path:suffix>/", ForumProxyAPIView.as_view(), name="forum_proxy"),
-    path("api/v2/threads/<str:thread_id>/votes/", ThreadVoteView.as_view(), name="thread-vote"),
-    path("api/v2/comments/<str:comment_id>/votes/", CommentVoteView.as_view(), name="comment-vote"),
+    path("api/v2/threads/<str:thread_id>/votes", ThreadVoteView.as_view(), name="thread-vote"),
+    path("api/v2/comments/<str:comment_id>/votes", CommentVoteView.as_view(), name="comment-vote"),
 ]
