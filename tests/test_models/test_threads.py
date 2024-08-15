56496bf82161c48ed7e46b9f2da46b3990a8bcb9
Régis Behmo <regis@behmo.com>
2024-08-15 11:18:12 +0200

    chore: refactor MongoBackend to simplify mocking
    The MongoBackend now provides access only to a database, and it is the
    responsibility of each child model to access the collection. This is
    done transparently via the _collection property.
    
    The MongoBackend constructor was also simplified. Because we are now
    using a single database object, we can drastically simplify mocking in
    tests. That's because we don't have to mock models one by one.
    
    And it turns out that we don't need to store the client attribute. Neither do we
    need to store a diffent collection name for every model instance, so we
    can simply store it as a class attribute. This means that we no longer
    need constructors for models.

--- tests/test_models/test_threads.py
+++ tests/test_models/test_threads.py
@@ -6,9 +6,9 @@ Tests for the `CommentThread` model.
 from forum.models import CommentThread
 
 
-def test_insert(comment_thread_model: CommentThread) -> None:
+def test_insert() -> None:
     """Test insert a comment thread into MongoDB."""
-    thread_id = comment_thread_model.insert(
+    thread_id = CommentThread().insert(
         title="Test Thread",
         body="This is a test thread",
         course_id="course1",
@@ -17,15 +17,15 @@ def test_insert(comment_thread_model: CommentThread) -> None:
         author_username="author_user",
     )
     assert thread_id is not None
-    thread_data = comment_thread_model.get(thread_id)
+    thread_data = CommentThread().get(thread_id)
     assert thread_data is not None
     assert thread_data["title"] == "Test Thread"
     assert thread_data["body"] == "This is a test thread"
 
 
-def test_delete(comment_thread_model: CommentThread) -> None:
+def test_delete() -> None:
     """Test delete a comment thread from MongoDB."""
-    thread_id = comment_thread_model.insert(
+    thread_id = CommentThread().insert(
         title="Test Thread",
         body="This is a test thread",
         course_id="course1",
@@ -33,15 +33,15 @@ def test_delete(comment_thread_model: CommentThread) -> None:
         author_id="author1",
         author_username="author_user",
     )
-    result = comment_thread_model.delete(thread_id)
+    result = CommentThread().delete(thread_id)
     assert result == 1
-    thread_data = comment_thread_model.get(thread_id)
+    thread_data = CommentThread().get(thread_id)
     assert thread_data is None
 
 
-def test_list(comment_thread_model: CommentThread) -> None:
+def test_list() -> None:
     """Test list all comment threads from MongoDB."""
-    comment_thread_model.insert(
+    CommentThread().insert(
         "Thread 1",
         "Body 1",
         "_type",
@@ -49,7 +49,7 @@ def test_list(comment_thread_model: CommentThread) -> None:
         "1",
         "user1",
     )
-    comment_thread_model.insert(
+    CommentThread().insert(
         "Thread 2",
         "Body 2",
         "_type",
@@ -57,7 +57,7 @@ def test_list(comment_thread_model: CommentThread) -> None:
         "1",
         "user1",
     )
-    comment_thread_model.insert(
+    CommentThread().insert(
         "Thread 3",
         "Body 3",
         "_type",
@@ -65,14 +65,14 @@ def test_list(comment_thread_model: CommentThread) -> None:
         "1",
         "user1",
     )
-    threads_list = comment_thread_model.list()
+    threads_list = CommentThread().list()
     assert len(list(threads_list)) == 3
     assert all(thread["title"].startswith("Thread") for thread in threads_list)
 
 
-def test_update(comment_thread_model: CommentThread) -> None:
+def test_update() -> None:
     """Test update a comment thread in MongoDB."""
-    thread_id = comment_thread_model.insert(
+    thread_id = CommentThread().insert(
         title="Test Thread",
         body="This is a test thread",
         course_id="course1",
@@ -81,14 +81,14 @@ def test_update(comment_thread_model: CommentThread) -> None:
         author_username="author_user",
     )
 
-    result = comment_thread_model.update(
+    result = CommentThread().update(
         thread_id=thread_id,
         title="Updated Title",
         body="Updated body",
         commentable_id="new_commentable_id",
     )
     assert result == 1
-    thread_data = comment_thread_model.get(thread_id)
+    thread_data = CommentThread().get(thread_id)
     assert thread_data is not None
     assert thread_data["title"] == "Updated Title"
     assert thread_data["body"] == "Updated body"
