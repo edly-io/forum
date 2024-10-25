7916cccf8d7805c23ed9192f94c024b82f0b3aed
Muhammad Faraz  Maqsood <faraz.maqsood@192.168.10.102>
2024-10-25 15:19:21 +0500

    fix: pin pylint to the same version as that of edx As updating the pylint version to latest fails edx CI and If pinning it to the same version as it(forum) has before, it fails compiling the requirements in edx-platofrm So, adding the same constraint for pylint as edx-platform has for it. i.e. `pylint<2.16.0`

--- forum/utils.py
+++ forum/utils.py
@@ -174,14 +174,13 @@ def get_group_ids_from_params(params: dict[str, Any]) -> list[int]:
     """
     if "group_id" in params and "group_ids" in params:
         raise ValueError("Cannot specify both group_id and group_ids")
-    group_ids: str | list[str] = []
+    group_ids: str | list[int] = params.get("group_ids", [])
     if group_id := params.get("group_id"):
         return [int(group_id)]
-    elif group_ids := params.get("group_ids", []):
-        if isinstance(group_ids, str):
-            return [int(x) for x in group_ids.split(",")]
-        elif isinstance(group_ids, list):
-            return [int(x) for x in group_ids]
+    elif isinstance(group_ids, str):
+        group_ids = [int(x) for x in group_ids.split(",")]
+    elif isinstance(group_ids, list):
+        group_ids = [int(x) for x in group_ids]
     return group_ids
 
 
